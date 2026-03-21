ebc046f0883d952f4a35186dfe13f7e143b66e93
Rohan Sharma <sharoha@Rohans-MacBook-Pro.local>
2026-03-21 20:59:23 +0900

    automate lsp installtion using mason, mason-lspconfig, neovim-lspconfig, and mason-lspconfig-installer

--- init.lua
+++ init.lua
@@ -26,6 +26,18 @@ require("setup.keymap")
 require("setup.commands")
 
 vim.cmd([[
-  hi Normal guibg=NONE ctermbg=NONE
+   hi Normal guibg=NONE ctermbg=NONE
   hi NormalNC guibg=NONE ctermbg=NONE
 ]])
+
+-- lsp,
+-- make sure to have nvim-lspconfig(has the list of lsp configurations file), check their github
+require("mason").setup()
+
+-- used for mangaging lspconfig downlowad using mason
+require("mason-lspconfig").setup()
+
+-- used for managing auto download using mason annd is able to differenciate between lsp and linter
+require("mason-tool-installer").setup({
+    ensure_installed = { "lua_ls", "stylua" },
+})
