97afa53cf4c782e422114386fa6f18309be535c3
Rohan Sharma <sharmarohan136@gmail.com>
2026-03-24 23:52:34 +0900

    Remove comments from lua_ls

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -2,24 +2,17 @@ return {
     settings = {
         Lua = {
             runtime = {
-                -- Tell the language server which version of Lua you're using (most
-                -- likely LuaJIT in the case of Neovim)
-                version = "LuaJIT",
-                -- Tell the language server how to find Lua modules same way as Neovim
-                -- (see `:h lua-module-load`)
-                path = {
+               version = "LuaJIT",
+               path = {
                     "lua/?.lua",
                     "lua/?/init.lua",
                 },
             },
-            -- Make the server aware of Neovim runtime files
             workspace = {
                 checkThirdParty = false,
                 library = {
                     vim.env.VIMRUNTIME,
-                    -- Depending on the usage, you might want to add additional paths
-                    -- here.
-                    "${3rd}/luv/library",
+                   "${3rd}/luv/library",
                     "${3rd}/busted/library",
                 },
             },
